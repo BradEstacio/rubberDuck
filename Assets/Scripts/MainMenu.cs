d86cfcad2c680a14a4bcda59a7b10d34332311c5
BradEstacio <bradestacio@gmail.com>
2024-10-17 17:56:14 -0500

    Successful start menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,22 +8,26 @@ public class MainMenu : MonoBehaviour
     public GameObject StartMenuUI;
     public GameObject DroppedObject;
 
-    public float timeBetweenDrops = 1f;
+    public float timeBetweenDrops = .5f;
     float nextDrop;
 
     void Awake()
     {
         nextDrop = 1f;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButton(0) && (nextDrop < Time.time))
         {
-            nextDrop = Time.time + timeBetweenDrops;
             Debug.Log("Duck dropped.");
-            Instantiate(DroppedObject, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 90, 0)));
+            nextDrop = Time.time + timeBetweenDrops;
+            float randX = Random.Range(-10f, 10f);
+            float randY = Random.Range(-4f, 8f);
+            Instantiate(DroppedObject, new Vector3(randX, randY, 0), Quaternion.Euler(new Vector3(0, 90, 0)));
+            //GameObject Duck = Instantiate(DroppedObject, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 90, 0)));
+            //DuckPachinko DuckGame = Duck.GetComponent<DuckPachinko>();
         }
     }
 
